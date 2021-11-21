e567168c77d92f97d811be5ad02f1910b88d5244
Andy Nenthong <C0516154@intra.camosun.bc.ca>
2021-11-21 00:44:43 -0800

    Add project files.

---
+++ TimedMathQuiz/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿
+namespace TimedMathQuiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtAnswer1 = new System.Windows.Forms.TextBox();
+            this.txtAnswer2 = new System.Windows.Forms.TextBox();
+            this.txtAnswer3 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 16F);
+            this.label1.Location = new System.Drawing.Point(89, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 16F);
+            this.label2.Location = new System.Drawing.Point(208, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "+";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 16F);
+            this.label3.Location = new System.Drawing.Point(322, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 16F);
+            this.label4.Location = new System.Drawing.Point(447, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 26);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "=";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Consolas", 16F);
+            this.label5.Location = new System.Drawing.Point(92, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 26);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "?";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Consolas", 16F);
+            this.label6.Location = new System.Drawing.Point(211, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 26);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "+";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Consolas", 16F);
+            this.label7.Location = new System.Drawing.Point(325, 185);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 26);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "?";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Consolas", 16F);
+            this.label8.Location = new System.Drawing.Point(450, 185);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(24, 26);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "=";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Consolas", 16F);
+            this.label9.Location = new System.Drawing.Point(92, 276);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(24, 26);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "?";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Consolas", 16F);
+            this.label10.Location = new System.Drawing.Point(211, 276);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(24, 26);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "+";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Consolas", 16F);
+            this.label11.Location = new System.Drawing.Point(325, 276);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(24, 26);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "?";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Consolas", 16F);
+            this.label12.Location = new System.Drawing.Point(450, 276);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(24, 26);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "=";
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Consolas", 16F);
+            this.lblTimer.Location = new System.Drawing.Point(568, 32);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(60, 26);
+            this.lblTimer.TabIndex = 12;
+            this.lblTimer.Text = "0:00";
+            this.lblTimer.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Consolas", 16F);
+            this.lblMessage.Location = new System.Drawing.Point(401, 356);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(108, 26);
+            this.lblMessage.TabIndex = 13;
+            this.lblMessage.Text = "xxxxxxxx";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // txtAnswer1
+            // 
+            this.txtAnswer1.Font = new System.Drawing.Font("Consolas", 16F);
+            this.txtAnswer1.Location = new System.Drawing.Point(573, 89);
+            this.txtAnswer1.Name = "txtAnswer1";
+            this.txtAnswer1.Size = new System.Drawing.Size(100, 32);
+            this.txtAnswer1.TabIndex = 1;
+            this.txtAnswer1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtAnswer1.TextChanged += new System.EventHandler(this.txtAnswer1_TextChanged);
+            // 
+            // txtAnswer2
+            // 
+            this.txtAnswer2.Font = new System.Drawing.Font("Consolas", 16F);
+            this.txtAnswer2.Location = new System.Drawing.Point(573, 181);
+            this.txtAnswer2.Name = "txtAnswer2";
+            this.txtAnswer2.Size = new System.Drawing.Size(100, 32);
+            this.txtAnswer2.TabIndex = 2;
+            this.txtAnswer2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtAnswer2.TextChanged += new System.EventHandler(this.txtAnswer2_TextChanged);
+            // 
+            // txtAnswer3
+            // 
+            this.txtAnswer3.Font = new System.Drawing.Font("Consolas", 16F);
+            this.txtAnswer3.Location = new System.Drawing.Point(573, 273);
+            this.txtAnswer3.Name = "txtAnswer3";
+            this.txtAnswer3.Size = new System.Drawing.Size(100, 32);
+            this.txtAnswer3.TabIndex = 3;
+            this.txtAnswer3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtAnswer3.TextChanged += new System.EventHandler(this.txtAnswer3_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 16F);
+            this.button1.Location = new System.Drawing.Point(97, 346);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 45);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "RESET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 461);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtAnswer3);
+            this.Controls.Add(this.txtAnswer2);
+            this.Controls.Add(this.txtAnswer1);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Timed Math Quiz";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtAnswer1;
+        private System.Windows.Forms.TextBox txtAnswer2;
+        private System.Windows.Forms.TextBox txtAnswer3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TimedMathQuiz/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimedMathQuiz
+{
+    public partial class Form1 : Form
+    {
+        private int secondsCounter;
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateRandomAdditions();
+            StartTheTime();
+        }
+        private void StartTheTime()
+        {
+            timer1.Start();
+            secondsCounter = 0;
+            lblTimer.Text = secondsCounter.ToString() + " seconds";
+        }
+        private void GenerateRandomAdditions()
+        {
+            Random r = new Random();
+            label1.Text = r.Next(1, 11).ToString();
+            label3.Text = r.Next(1, 11).ToString();
+            label5.Text = r.Next(1, 11).ToString();
+            label7.Text = r.Next(1, 11).ToString();
+            label9.Text = r.Next(1, 11).ToString();
+            label11.Text = r.Next(1, 11).ToString();
+        }
+        private bool checkAnswers()
+        {
+            int answer1 = int.Parse(label1.Text) + int.Parse(label3.Text);
+            int answer2 = int.Parse(label5.Text) + int.Parse(label7.Text);
+            int answer3 = int.Parse(label9.Text) + int.Parse(label11.Text);
+
+            if (answer1.ToString()==txtAnswer1.Text && answer2.ToString() == txtAnswer2.Text && answer3.ToString() == txtAnswer3.Text)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            secondsCounter++;
+            lblTimer.Text = secondsCounter.ToString() + " seconds";
+        }
+        private void txtAnswer1_TextChanged(object sender, EventArgs e)
+        {
+            if (checkAnswers())
+            {
+                timer1.Stop();
+                lblMessage.Text = string.Format("Your time: {0} seconds.", secondsCounter);
+            }
+        }
+        private void txtAnswer2_TextChanged(object sender, EventArgs e)
+        {
+            if (checkAnswers())
+            {
+                timer1.Stop();
+                lblMessage.Text = string.Format("Your time: {0} seconds.", secondsCounter);
+            }
+        }
+
+        private void txtAnswer3_TextChanged(object sender, EventArgs e)
+        {
+            if (checkAnswers())
+            {
+                timer1.Stop();
+                lblMessage.Text = string.Format("Your time: {0} seconds.", secondsCounter);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GenerateRandomAdditions();
+            StartTheTime();
+            lblMessage.Text = "xxxxxxxx";
+            txtAnswer1.Clear();
+            txtAnswer2.Clear();
+            txtAnswer3.Clear();
+        }
+    }
+}
